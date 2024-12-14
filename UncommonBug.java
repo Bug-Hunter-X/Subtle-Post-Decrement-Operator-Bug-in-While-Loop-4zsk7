4ab1ad0c2fe3f3f75abf4003ec5fdab922380a2a
BugHunter-X <askzenai@gmail.com>
2024-12-15 00:30:07 +0100

    Add UncommonBug.java file

---
+++ UncommonBug.java
@@ -0,0 +1,12 @@
+public class UncommonBug {
+    public static void main(String[] args) {
+        int i = 10;
+        try {
+            while (i-->0) {
+                System.out.println(i); 
+            }
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
